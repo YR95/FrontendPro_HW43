b40b85cf398beab2d13fe93b6c9faad54e21c75f
yuliia <pustovayajulia@gmail.com>
2023-08-12 00:11:34 +0300

    Add table with visual

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 function renderForm() {
   let form = document.createElement("form");
 
-  addradiobutton(form, "text", "Name");
+  addElementinDom(form, "text", "Name");
 
-  addradiobutton(form, "text", "Surname");
-  addradiobutton(form, "date", "Date of birth");
-  addradiobutton(form, "radio", "Female");
-  addradiobutton(form, "radio", "Male");
-  addradiobutton(form, "checkbox", "English");
-  addradiobutton(form, "checkbox", "Ukranian");
+  addElementinDom(form, "text", "Surname");
+  addElementinDom(form, "date", "Date of birth");
+  addElementinDom(form, "radio", "Female");
+  addElementinDom(form, "radio", "Male");
+  addElementinDom(form, "checkbox", "English");
+  addElementinDom(form, "checkbox", "Ukranian");
   let addresa = document.createElement("textarea");
   addresa.id = "addresa";
   addresa.rows = 2;
@@ -30,16 +30,18 @@ function renderForm() {
 
     select.appendChild(option);
 
-    // select.addEventListener("change", e => {
-    //   console.log(e.target.value);
-    // });
   }
-  form.append(select, addresa);
+
+  let button = document.createElement("button");
+  button.id = "saveButton";
+  button.innerText = "Save data";
+
+  form.append(select, addresa, button);
 
   document.body.appendChild(form);
 }
 
-function addradiobutton(form, type, text) {
+function addElementinDom(form, type, text) {
   var label = document.createElement("label");
 
   var element = document.createElement("input");
@@ -102,7 +104,6 @@ function getValueFromActionListenerClick(id, object, field) {
 
   let result = "";
 
-  let fff = document.querySelector(id);
   document.querySelector(id) !== null ?
       document.querySelector(id).addEventListener("click", (ev) => {
         result = (ev.target.id);
@@ -116,7 +117,50 @@ function getValueFromActionListenerClick(id, object, field) {
       : "";
 }
 
+function clickButtonRendertAble(button, person) {
+
+  document.querySelector(button).addEventListener("click", (ev) => {
+    document.querySelector("form").innerHTML = "";
+    let table = document.createElement("table");
+    let tr = document.createElement("tr");
+
+    let tdName = document.createElement("td");
+    tdName.innerHTML = "Name: " + person.name;
+    let tdSurname = document.createElement("td");
+    tdSurname.innerHTML = "Surname :" + person.surname;
+    let trSecond = document.createElement("tr");
+
+    let tdadressa = document.createElement("td");
+    tdadressa.innerHTML = "Address :" + person.adressa;
+
+    let tdgender = document.createElement("td");
+    tdgender.innerHTML = "Gender :" + person.gender;
+
+    let trThird = document.createElement("tr");
+    let tdlanguages = document.createElement("td");
+    tdlanguages.innerHTML = "Languages :" + person.languages;
+
+    let tdcity = document.createElement("td");
+    tdcity.innerHTML = "City :" + person.city;
+
+    let tdabirth = document.createElement("td");
+    tdabirth.innerHTML = "Birth :" + person.dataOfBirth;
+
+    tr.appendChild(tdName);
+    tr.appendChild(tdSurname);
+    trSecond.appendChild(tdadressa);
+    trSecond.appendChild(tdgender);
+
+    trThird.appendChild(tdlanguages);
+    trThird.appendChild(tdcity);
+
+    table.append(tr, trSecond, trThird);
+    document.body.appendChild(table);
+  });
+}
+
 personAm = new Person();
 renderForm(personAm);
 getValuesFromInputs(personAm);
 console.log(personAm);
+clickButtonRendertAble("#saveButton", personAm);
