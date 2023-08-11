a224fe78682739b7cb73b0a235377e61945b4df5
yuliia <pustovayajulia@gmail.com>
2023-08-11 23:22:20 +0300

    Add for all actionListeners

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ function renderForm() {
   let form = document.createElement("form");
 
   addradiobutton(form, "text", "Name");
+
   addradiobutton(form, "text", "Surname");
-  addradiobutton(form, "text", "Date of birth");
+  addradiobutton(form, "date", "Date of birth");
   addradiobutton(form, "radio", "Female");
   addradiobutton(form, "radio", "Male");
   addradiobutton(form, "checkbox", "English");
@@ -29,25 +30,27 @@ function renderForm() {
 
     select.appendChild(option);
 
-    select.addEventListener("change", e => {
-      console.log(e.target.value);
-    });
+    // select.addEventListener("change", e => {
+    //   console.log(e.target.value);
+    // });
   }
   form.append(select, addresa);
 
   document.body.appendChild(form);
-
 }
 
 function addradiobutton(form, type, text) {
   var label = document.createElement("label");
 
   var element = document.createElement("input");
-  //Assign different attributes to the element.
   element.setAttribute("type", type);
-  element.setAttribute("value", type);
+  element.setAttribute("value", text);
   element.setAttribute("name", type);
   element.setAttribute("id", text);
+
+  if (text === "Name" || text === "Surname") {
+    element.setAttribute("value", "");
+  }
 
   label.appendChild(element);
   label.innerHTML += text;
@@ -57,4 +60,63 @@ function addradiobutton(form, type, text) {
   return element;
 }
 
-renderForm();
+function getValuesFromInputs(person) {
+  getValueFromActionListener("#Name", person, "name");
+  getValueFromActionListener("#Surname", person, "surname");
+  getValueFromActionListener("input[name=date]", person, "dataOfBirth");
+  getValueFromActionListenerClick('#Female', person, "gender");
+  getValueFromActionListenerClick('#Male', person, "gender");
+  getValueFromActionListenerClick("#English", person, "languages");
+  getValueFromActionListenerClick("#Ukranian", person, "languages");
+  getValueFromActionListener("#addresa", person, "adressa");
+  getValueFromActionListener("select", person, "city");
+
+  return person;
+}
+
+class Person {
+  name = "";
+  surname = "";
+  adressa = "";
+  city = "";
+  gender = "";
+  languages = [];
+  dataOfBirth = "";
+
+}
+
+function getValueFromActionListener(id, object, field) {
+
+  let result = "";
+  document.querySelector(id) !== null ?
+      document.querySelector(id).addEventListener("change", (ev) => {
+        result = ev.target.value;
+        object[field] = result;
+        console.log(object);
+      })
+      : "";
+
+}
+
+function getValueFromActionListenerClick(id, object, field) {
+
+  let result = "";
+
+  let fff = document.querySelector(id);
+  document.querySelector(id) !== null ?
+      document.querySelector(id).addEventListener("click", (ev) => {
+        result = (ev.target.id);
+        if (field === "languages") {
+          object[field].push(result);
+        } else {
+          object[field] = result;
+        }
+        console.log(object);
+      })
+      : "";
+}
+
+personAm = new Person();
+renderForm(personAm);
+getValuesFromInputs(personAm);
+console.log(personAm);
